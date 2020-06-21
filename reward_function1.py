d4ec92052da4daf91879f606c6c1c4676281d9f8
Naveen Ghanta <naveen.205@gmail.com>
2020-06-21 09:20:23 -0400

    added reward for maintaining close to the center line

--- reward_function1.py
+++ reward_function1.py
@@ -30,6 +30,21 @@ def reward_function(params):
     else:
         reward += 2.0
 
+    # Calculate 3 markers that are increasingly further away from the center line
+    marker_1 = 0.1 * track_width
+    marker_2 = 0.25 * track_width
+    marker_3 = 0.5 * track_width
+
+    # Give higher reward if the car is closer to center line and vice versa
+    if distance_from_center <= marker_1:
+        reward += 10
+    elif distance_from_center <= marker_2:
+        reward += 5
+    elif distance_from_center <= marker_3:
+        reward += 1
+    else:
+        reward -= 10  # likely crashed/ close to off track
+
     # Calculate the direction of the center line based on the closest waypoints
     next_point = waypoints[closest_waypoints[1]]
     prev_point = waypoints[closest_waypoints[0]]
